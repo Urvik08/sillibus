650ec32d8ac1941957385d879c20a79271955ea7
Murtaza <MurtazaB@gatech.edu>
2016-01-16 17:26:53 -0500

    Added All Bootstrap Files
    all the files for the webapp

---
+++ All Bootstrap Files/js/app.js
@@ -0,0 +1,29 @@
+var dropbox;
+function init() {
+dropbox = document.getElementById("dropbox");
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+}
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  var dt = e.dataTransfer;
+  var files = dt.files;
+
+  var form = document.getElementById("fileUploader");
+  form.file.files = files;
+  form.submit();
+}
+window.onload = init;
